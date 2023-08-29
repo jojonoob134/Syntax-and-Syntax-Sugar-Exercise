77dddd402f90e7eb85e0c09b52a4f8f04f0b2148
Jorge Saldana <jojonoob134@gmail.com>
2023-08-29 14:48:30 -0500

    made changes

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var movie = "scott pillgram vs the world";
+            var times = 7;
+            var clock = "7:30";
+            Console.WriteLine($"My absolute favoret movie is {movie} and I watched it about {times} and i'm going to watch it again at {clock}pm");
+
+            string interpo = (movie == "scott pillgram vs the world") ? "yes scott pillgram is my favoret movie to" : "no that's not my favoret moive";
+            Console.WriteLine(interpo);
         }
     }
 }
